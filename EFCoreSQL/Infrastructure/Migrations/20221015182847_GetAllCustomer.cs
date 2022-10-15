5cdf718ff3b9650970fab45f8261cd9bd103c345
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-10-15 22:12:45 +0330

    Create migration SQLView for GetAllCustomer

---
+++ EFCoreSQL/Infrastructure/Migrations/20221015182847_GetAllCustomer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreSQL.Infrastructure.Migrations
+{
+    public partial class GetAllCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE VIEW GetAllCustomer as" +
+                                "SELECT Customers.Id, Customers.Name" +
+                                "FROM dbo.Customers;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW GetAllCustomer;");
+        }
+    }
+}
