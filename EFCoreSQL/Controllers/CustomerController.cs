74d8b3b5dbf7b6e946674c53d2fb84c1efa02660
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-10-15 22:21:09 +0330

    Use GetAllCustomer SQLVIEW

---
+++ EFCoreSQL/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using EFCoreSQL.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreSQL.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private AppDbContext _context;
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetAllCustomer()
+        {
+            var customers = _context.GetAllCustomers.ToList();
+            return Ok(customers);
+        }
+    }
+}
