766c036de66abaedc0eb7c941936b8168bcc735f
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-10-27 21:51:47 +0330

    Add order entity

---
+++ EFCoreSQL/Domain/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreSQL.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
